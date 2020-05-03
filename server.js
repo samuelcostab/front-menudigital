56bcedcbc559d9db31d4b69a0f5948ca4a70199e
samuelcostab <samuelufcbrito@gmail.com>
2020-05-03 13:17:03 -0300

    Subindo pra net

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-app.use('/miniburg', express.static(path.join(__dirname, '/build')));
+
+app.use(express.static(path.join(__dirname,"build")));
 
 
 const port = process.env.PORT || 3000;
