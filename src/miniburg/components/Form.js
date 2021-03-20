2a1eae8ff20d4db8284b13af9de045165db5fbff
Samuel Costa <39920166+samuelcostab@users.noreply.github.com>
2021-03-20 08:45:58 -0300

    Atualizando precos

--- src/miniburg/components/Form.js
+++ src/miniburg/components/Form.js
@@ -13,73 +13,73 @@ const burgues = [
     {
         item: "MINIHULK",
         ingredients: "PÃO ÁRABE, CARNE, QUEIJO, PRESUNTO, OVO, CALABRESA, FRANGO, BACON, ALFACE, CEBOLA, KETCHUP E MOLHO MINIBURG",
-        valorP:0, valorM:0, valorG:13.00
+        valorP:0, valorM:0, valorG:14.00
     },
     {
         item: "AVANTE",
         ingredients: "PÃO, CARNE, MUSSARELA, QUEIJO COALHO, PRESUNTO PERU, PRESUNTO CHESTER, BARBECUE, ALFACE E MOLHO MINIBURG",
-        valorP:7.00, valorM:8.50, valorG:10.00
+        valorP:7.50, valorM:9.50, valorG:11.00
     },
     {
         item: "MINIMODA ",
         ingredients: "PÃO, CARNE, QUEIJO, PRESUNTO, OVO, CALABRESA, ALFACE, CEBOLA, KETCHUP E MOLHO MINIBURG",
-        valorP:7.00, valorM:9.00, valorG:10.00
+        valorP:7.50, valorM:10.00, valorG:11.00
     },
     {
         item: "EGUICALÍ",
         ingredients: "PÃO, CARNE, QUEIJO, PRESUNTO, OVO, ALFACE, CEBOLA, KETCHUP E MOLHO MINIBURG",
-        valorP:6.00, valorM:8.00, valorG:8.50
+        valorP:6.50, valorM:9.00, valorG:9.50
     },
     {
         item: "AMERICANO",
         ingredients: "PÃO, CARNE, BACON, QUEIJO, ALFACE, CEBOLA, KETCHUP E MOLHO MINIBURG",
-        valorP:5.00, valorM:7.00, valorG:8.00
+        valorP:5.50, valorM:8.00, valorG:9.00
     },
     {
         item: "BRASILIANO",
         ingredients: "PÃO, FRANGO, QUEIJO, OVO, ALFACE, CEBOLA, KETCHUP E MOLHO MINIBURG",
-        valorP:5.00, valorM:7.00, valorG:8.00
+        valorP:5.50, valorM:8.00, valorG:9.00
     },
     {
         item: "MINIFIT",
         ingredients: "PÃO INTEGRAL, FRANGO, OVO, QUEIJO, ALFACE, CEBOLA E MOLHO MINIBURG",
-        valorP:0, valorM: 6.50, valorG:0,
+        valorP:0, valorM: 7.50, valorG:0,
     },
 
     {
         item: "CALIFÓRNIA",
         ingredients: "PÃO, CARNE, QUEIJO, PRESUNTO, ALFACE, CEBOLA, KETCHUP E MOLHO MINIBURG",
-        valorP:5.00, valorM:7.00, valorG:8.00
+        valorP:5.50, valorM:8.00, valorG:9.00
     },
     {
         item: "CALABOM",
         ingredients: "PÃO, CALABRESA, QUEIJO, PRESUNTO, ALFACE, CEBOLA, KETCHUP E MOLHO MINIBURG",
-        valorP:5.00, valorM:7.00, valorG:8.00
+        valorP:5.50, valorM:8.00, valorG:9.00
     },
     {
         item: "EGUIBÚ",
         ingredients: "PÃO, CARNE, OVO, ALFACE, CEBOLA, KETCHUP E MOLHO MINIBURG",
-        valorP:4.50, valorM:6.00, valorG:7.00
+        valorP:5.00, valorM:7.00, valorG:8.00
     },
     {
         item: "PRESBURG",
         ingredients: "PÃO, CARNE, PRESUNTO, ALFACE, CEBOLA, KETCHUP E MOLHO MINIBURG",
-        valorP:4.50, valorM:6.00, valorG:6.50
+        valorP:5.00, valorM:7.00, valorG:7.50
     },
     {
         item: "XÍSBOM",
         ingredients: "PÃO, CARNE, QUEIJO, ALFACE, CEBOLA, KETCHUP E MOLHO MINIBURG",
-        valorP:4.50, valorM:6.00, valorG:6.50
+        valorP:5.00, valorM:7.00, valorG:7.50
     },
     {
         item: "SIMPRÃO",
         ingredients: "PÃO, CARNE, ALFACE, CEBOLA, KETCHUP E MOLHO MINIBURG",
-        valorP:3.50, valorM:4.50, valorG:5.50
+        valorP:4.00, valorM:5.50, valorG:6.50
     },
     {
         item: "MIX",
         ingredients: "PÃO, QUEIJO, PRESUNTO, ALFACE, CEBOLA, KETCHUP E MOLHO MINIBURG",
-        valorP:3.50, valorM:4.50, valorG:5.50
+        valorP:4.00, valorM:5.50, valorG:6.50
     },
 ]
 
